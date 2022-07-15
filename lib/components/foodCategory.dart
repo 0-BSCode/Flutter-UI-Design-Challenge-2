110d976b6538d40f9a6433ad8b96bb3baa3590ea
Bryan Sanchez <bryanipsanchez@gmail.com>
2022-07-15 14:14:04 +0800

    Finished home page

---
+++ lib/components/foodCategory.dart
@@ -0,0 +1,11 @@
+import 'package:flutter/material.dart';
+
+class FoodCategory extends StatelessWidget {
+  final dynamic itemData;
+  const FoodCategory({Key? key, required this.itemData}) : super(key: key);
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
